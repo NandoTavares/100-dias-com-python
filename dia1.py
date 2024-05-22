928a7e6ed5682d3980be67058bf4b76c03995aac
Ernandes Tavares <131466882+NandoTavares@users.noreply.github.com>
2024-05-22 08:02:17 -0300

    Create dia1.py

---
+++ dia1.py
@@ -0,0 +1,4 @@
+print("Nome de Banda Gerador")
+mystring = input("Diga o nome da cidade que você nasceu: ")
+mystringpet = input("Diga o nome do seu pet: ")
+print("nome da sua banda vai ser: ", mystring + mystringpet)
